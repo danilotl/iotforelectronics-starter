1a7c8a4ae5f8cad730e05be6875839cd8ecfba56
Alexis R. Silva <alexisrs@br.ibm.com>
2016-04-02 16:55:12 -0300

    Testing integration errors #3

--- app/app.js
+++ app/app.js
@@ -38,124 +38,11 @@ if(process.env.VCAP_SERVICES)
 else if (iot4electronicsDebug && iot4electronicsDebug.VCAP_SERVICES)
 	VCAP_SERVICES = iot4electronicsDebug.VCAP_SERVICES;
 
-
-
 //The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 
 //global connectedDevicesCache
 //connectedDevicesCache = require('./lib/connectedDevicesCache');
-
-
-
-//global HTTP routers
-httpRouter = require('./routes/httpRouter');
-
-//get IoT-Foundation credentials
-if(!VCAP_SERVICES || !VCAP_SERVICES["iotf-service"])
-	throw "Cannot get IoT-Foundation credentials"
-var iotfCredentials = VCAP_SERVICES["iotf-service"][0]["credentials"];
-
-//global IoT-Foundation connectors 
-washingMachineIoTFClient = require('./mqtt/washingMachineIoTFClient'); 
-washingMachineIoTFClient.connectToBroker(iotfCredentials);
-	
-var app = express();
-
-//Enable reverse proxy support in Express. This causes the
-//the "X-Forwarded-Proto" header field to be trusted so its
-//value can be used to determine the protocol. See 
-//http://expressjs.com/api#app-settings for more details.
-app.enable('trust proxy');
-
-//Add a handler to inspect the req.secure flag (see 
-//http://expressjs.com/api#req.secure). This allows us 
-//to know whether the request was via http or https.
-app.use (function (req, res, next) {
-     if (req.secure || app.get('env') === 'development') {
-             // request was via https, so do no special handling
-             next();
-     } else {
-             // request was via http, so redirect to https
-             res.redirect('https://' + req.headers.host + req.url);
-     }
-});
-
-//set the app object to export so it can be required 
-module.exports = app;
-var server = require('http').Server(app);
-iotAppMonitor = require('./lib/iotAppMonitorServer')(server);
-
-//view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-//uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-//allow cross domain calls
-app.use(cors());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/', httpRouter);
-app.use('/', device);
-app.use('/', simulator);
-
-//iot-workbench additional requires   
-try {
-	require("./_requires.js");	
-} catch (e) {	
-		//no iot-workbench additional _requires;		
-}
-
-
-//catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-});
-
-//error handlers
-
-//development error handler
-//will print stacktrace
-if (app.get('env') === 'development') {
-	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: err
-		});
-	});
-}
-
-//production error handler
-//no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: {}
-	});
-});
-
-app.set('port', process.env.VCAP_APP_PORT || 3000);
-
-//require user extensions  
-try {
-	require("./iot4electronics.js");	
-} catch (e) {
-	try {
-		require("./_app.js");			
-	} catch (e) {
-		console.log("Failed to load extention files _app.js or iot4electronics.js: " + e.message);
-	};	
-}
 
 /***************************************************************/
 //STEPHANIES'S CODE *************
@@ -701,6 +588,115 @@ app.delete("/user/:userID", passport.authenticate('mca-backend-strategy', {sessi
      res.sendStatus(204);
   });
 });
+
+//global HTTP routers
+httpRouter = require('./routes/httpRouter');
+
+//get IoT-Foundation credentials
+if(!VCAP_SERVICES || !VCAP_SERVICES["iotf-service"])
+	throw "Cannot get IoT-Foundation credentials"
+var iotfCredentials = VCAP_SERVICES["iotf-service"][0]["credentials"];
+
+//global IoT-Foundation connectors 
+washingMachineIoTFClient = require('./mqtt/washingMachineIoTFClient'); 
+washingMachineIoTFClient.connectToBroker(iotfCredentials);
+	
+var app = express();
+
+//Enable reverse proxy support in Express. This causes the
+//the "X-Forwarded-Proto" header field to be trusted so its
+//value can be used to determine the protocol. See 
+//http://expressjs.com/api#app-settings for more details.
+app.enable('trust proxy');
+
+//Add a handler to inspect the req.secure flag (see 
+//http://expressjs.com/api#req.secure). This allows us 
+//to know whether the request was via http or https.
+app.use (function (req, res, next) {
+     if (req.secure || app.get('env') === 'development') {
+             // request was via https, so do no special handling
+             next();
+     } else {
+             // request was via http, so redirect to https
+             res.redirect('https://' + req.headers.host + req.url);
+     }
+});
+
+//set the app object to export so it can be required 
+module.exports = app;
+var server = require('http').Server(app);
+iotAppMonitor = require('./lib/iotAppMonitorServer')(server);
+
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+//allow cross domain calls
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/', httpRouter);
+app.use('/', device);
+app.use('/', simulator);
+
+//iot-workbench additional requires   
+try {
+	require("./_requires.js");	
+} catch (e) {	
+		//no iot-workbench additional _requires;		
+}
+
+
+//catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+//error handlers
+
+//development error handler
+//will print stacktrace
+if (app.get('env') === 'development') {
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
+}
+
+//production error handler
+//no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
+});
+
+app.set('port', process.env.VCAP_APP_PORT || 3000);
+
+//require user extensions  
+try {
+	require("./iot4electronics.js");	
+} catch (e) {
+	try {
+		require("./_app.js");			
+	} catch (e) {
+		console.log("Failed to load extention files _app.js or iot4electronics.js: " + e.message);
+	};	
+}
 
 //Start server
 server.listen(app.get('port'), function() {
