3007e2207a163ca214d001e91a6803d9d41c063b
Alexis R. Silva <alexisrs@br.ibm.com>
2016-04-09 20:49:25 -0300

    changing code order to test Registration#5

--- app/app.js
+++ app/app.js
@@ -1025,69 +1025,69 @@ function onListening() {
  */
 
 //Using hardcoded user repository
-// var userRepository = {
-//     "conrad":      { password: "12345" , displayName:"Conrad Kao"      , dob:"October 9, 1940"},
-//     "john.lennon":      { password: "12345" , displayName:"John Lennon"      , dob:"October 9, 1940"},
-//     "paul.mccartney":   { password: "67890" , displayName:"Paul McCartney"   , dob:"June 18, 1942"},
-//     "ringo.starr":      { password: "abcde" , displayName:"Ringo Starr"      , dob: "July 7, 1940"},
-//     "george.harrison":  { password: "fghij" , displayName: "George Harrison" , dob:"Feburary 25, 1943"}
-// };
+var userRepository = {
+    "conrad":      { password: "12345" , displayName:"Conrad Kao"      , dob:"October 9, 1940"},
+    "john.lennon":      { password: "12345" , displayName:"John Lennon"      , dob:"October 9, 1940"},
+    "paul.mccartney":   { password: "67890" , displayName:"Paul McCartney"   , dob:"June 18, 1942"},
+    "ringo.starr":      { password: "abcde" , displayName:"Ringo Starr"      , dob: "July 7, 1940"},
+    "george.harrison":  { password: "fghij" , displayName: "George Harrison" , dob:"Feburary 25, 1943"}
+};
 
-// var logger = log4js.getLogger("CustomIdentityProviderApp");
-// logger.info("Starting up");
+var logger = log4js.getLogger("CustomIdentityProviderApp");
+logger.info("Starting up");
 
-// app.post('/apps/:tenantId/:realmName/startAuthorization', jsonParser, function(req, res){
-//     var tenantId = req.params.tenantId;
-//     var realmName = req.params.realmName;
-//     var headers = req.body.headers;
+app.post('/apps/:tenantId/:realmName/startAuthorization', jsonParser, function(req, res){
+    var tenantId = req.params.tenantId;
+    var realmName = req.params.realmName;
+    var headers = req.body.headers;
 
-//     logger.debug("startAuthorization", tenantId, realmName, headers);
+    logger.debug("startAuthorization", tenantId, realmName, headers);
 
-//     var responseJson = {
-//         status: "challenge",
-//         challenge: {
-//             text: "Enter username and password"
-//         }
-//     };
+    var responseJson = {
+        status: "challenge",
+        challenge: {
+            text: "Enter username and password"
+        }
+    };
 
-//     res.status(200).json(responseJson);
-// });
+    res.status(200).json(responseJson);
+});
 
-// app.post('/apps/:tenantId/:realmName/handleChallengeAnswer', jsonParser, function(req, res){
-//     var tenantId = req.params.tenantId;
-//     var realmName = req.params.realmName;
-//     var challengeAnswer = req.body.challengeAnswer;
+app.post('/apps/:tenantId/:realmName/handleChallengeAnswer', jsonParser, function(req, res){
+    var tenantId = req.params.tenantId;
+    var realmName = req.params.realmName;
+    var challengeAnswer = req.body.challengeAnswer;
 
-//     logger.debug("handleChallengeAnswer", tenantId, realmName, challengeAnswer);
+    logger.debug("handleChallengeAnswer", tenantId, realmName, challengeAnswer);
 
-//     var username = req.body.challengeAnswer["username"];
-//     var password = req.body.challengeAnswer["password"];
+    var username = req.body.challengeAnswer["username"];
+    var password = req.body.challengeAnswer["password"];
 
-//     var responseJson = { status: "failure" };
+    var responseJson = { status: "failure" };
 
-//     //add the following lines to add a new user (temporily) when the username is not existed.
-//     if (userRepository[username] == null) {
-//         userRepository[username]={password: password, displayName: username, dob:"December 31, 2016"};
-//     }
+    //add the following lines to add a new user (temporily) when the username is not existed.
+    if (userRepository[username] == null) {
+        userRepository[username]={password: password, displayName: username, dob:"December 31, 2016"};
+    }
 
-//     var userObject = userRepository[username];
+    var userObject = userRepository[username];
 
-//     if (userObject && userObject.password == password ){
-//         logger.debug("Login success for userId ::", username);
-//         responseJson.status = "success";
-//         responseJson.userIdentity = {
-//             userName: username,
-//             displayName: userObject.displayName,
-//             attributes: {
-//                 dob: userObject.dob
-//             }
-//         };
-//     } else {
-//         logger.debug("Login failure for userId ::", username);
-//     }
+    if (userObject && userObject.password == password ){
+        logger.debug("Login success for userId ::", username);
+        responseJson.status = "success";
+        responseJson.userIdentity = {
+            userName: username,
+            displayName: userObject.displayName,
+            attributes: {
+                dob: userObject.dob
+            }
+        };
+    } else {
+        logger.debug("Login failure for userId ::", username);
+    }
 
-//     res.status(200).json(responseJson);
-// });
+    res.status(200).json(responseJson);
+});
 
 // app.use(function(req, res, next){
 //     res.status(404).send("This is not the URL you're looking for");
