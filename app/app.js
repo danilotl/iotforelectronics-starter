dac6a9ff27e22a4fc9cfdb2cb65dcad70a56806e
stephwil <stephwil@us.ibm.com>
2016-05-10 06:28:08 -0400

    Update app.js

--- app/app.js
+++ app/app.js
@@ -214,7 +214,7 @@ app.get('/users/:userID', passport.authenticate('mca-backend-strategy', {session
 	    if (!error && response.statusCode == 200) {
         	// Print out the response body
         	console.log(body)
-        	}else (){
+        	}else{
         	console.log(error)
         	}
         		
@@ -240,7 +240,7 @@ app.get('/usersTest/:userID', function(req, res)
 	    if (!error && response.statusCode == 200) {
         	// Print out the response body
         	console.log(body)
-        	}else (){
+        	}else{
         	console.log(error)
         	}
         		
