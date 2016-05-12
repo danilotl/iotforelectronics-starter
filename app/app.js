be83ff5efea55dd66bea140b894b48d70880f758
Alexis R. Silva <alexisrs@br.ibm.com>
2016-05-12 11:16:58 -0300

    Adding code to test API validation.#4

--- app/app.js
+++ app/app.js
@@ -543,8 +543,6 @@ request
   .on('error', function(error){
     if(error.code === 'ETIMEDOUT')
       console.log('Request timed out.');
-    else if(error.code === 'ECONNREFUSED')
-      console.log('Connection Refused.');
     else
       console.log(error);
   }); 
@@ -555,18 +553,18 @@ console.log('called api to Store IOTP Credentials on : ' + 'https://iotforelectr
 /* Route to show one user doc using Cloudant Query             */
 /* Takes a userID in the url params                            */
 /***************************************************************/
-//app.get('/validation', function(req, res)
-//{
-//	res.redirect('https://iotforelectronicstile.stage1.mybluemix.net/validation/' + iotETenant + '/' +  iotEAuthToken + '/' + iotEApiKey, function (reqest, response){
-//	if (response.statusCode == 201) {
-//			res.sendStatus(httpResponse.statusCode);
-//			console.log("SUCCESS: " + bodyIn);
-//	} else {
-//			console.log("Error in POST /appliances" + response.statusCode);
-//			res.sendStatus(httpResponse.statusCode);
-//	}
-//});
-//});
+app.get('/validation', function(req, res)
+{
+	res.redirect('https://iotforelectronicstile.stage1.mybluemix.net/validation/' + iotETenant + '/' +  iotEAuthToken + '/' + iotEApiKey, function (reqest, response){
+	if (response.statusCode == 201) {
+			res.sendStatus(httpResponse.statusCode);
+			console.log("SUCCESS: " + bodyIn);
+	} else {
+			console.log("Error in POST /appliances" + response.statusCode);
+			res.sendStatus(httpResponse.statusCode);
+	}
+});
+});
 
 // //var iotePass = ioteCredentials["password"];
 
