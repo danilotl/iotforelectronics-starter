5482ce40bb689ce34ddd2bc8957e3c3e77be060d
stephwil <stephwil@us.ibm.com>
2016-05-24 08:55:39 -0400

    taking comma out

--- app/app.js
+++ app/app.js
@@ -383,7 +383,7 @@ app.post("/usersTest", function (req, res)
 		console.log('JSON log should have been sent');
 	}
 	request({
-   		url: 'https://iotforelectronicstile.stage1.mybluemix.net/users/internalSteph/'+ iotETenant,+ '/' + iotEApiKey + '/' + iotEAuthToken,
+   		url: 'https://iotforelectronicstile.stage1.mybluemix.net/users/internalSteph/'+ iotETenant + '/' + iotEApiKey + '/' + iotEAuthToken,
    		json: formData,
 		method: 'POST', 
 		headers: {
