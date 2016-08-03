d5844eada7e03471e5284153cad9be7599d5bf90
Danilo Teixeira Lopes <daniloteixeiralopes@gmail.com>
2016-08-03 11:30:53 -0300

    Adding 'helmet' package for Security Best Practices

--- app/app.js
+++ app/app.js
@@ -34,7 +34,8 @@ var path            = require('path'),
     request         = require('request'),
     _               = require("underscore"),
     appEnv          = cfenv.getAppEnv(),
-    q               = require('q');
+    q               = require('q'),
+    helmet			= require('helmet');
 
 var jsonParser = bodyParser.json();
 var i18n = require("i18n");
@@ -91,6 +92,7 @@ app.use(function (req, res, next) {
 });
 
 //allow cross domain calls
+app.use(helmet());
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
