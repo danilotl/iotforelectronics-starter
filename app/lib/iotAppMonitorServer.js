8b71b5e3fcbda99e74b3a8f9b06aaa18aa027fa6
Danilo Teixeira Lopes <daniloteixeiralopes@gmail.com>
2016-11-30 13:42:08 -0200

    Fix for socket.io issues found in webscan

--- app/lib/iotAppMonitorServer.js
+++ app/lib/iotAppMonitorServer.js
@@ -4,11 +4,21 @@
 
 module.exports = function (server) {
 	
-	var io = require('socket.io')(server, {
-			  serveClient: true,
-			  path: '/socket.io'
-			});
+	var io = require('socket.io')(server);
+	
 	io.set('transports', ['websocket']);
+
+	io.use(function(socket, next){
+		url = socket.request.url;
+		
+		if(url.indexOf('/socket.io/?EIO=') == -1){
+			socket.error('Bad Request');
+  			socket.disconnect(true);
+  			next(new Error('Bad Request'));
+		} else {
+			next();
+		}
+	});
 
 	io.on('connection', function(socket){
 		//socket.emit('iotwb-http', { message: 'Connected to application. Waiting for http calls.' });
